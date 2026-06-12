2284ecd43ba325efabb48ef4ecb5c342d8bce299
nonoskillers <noahbehar20@gmail.com>
2026-06-12 13:57:45 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,13 +1,9 @@
-const CACHE="rearise-v1";
-self.addEventListener("install",function(e){
-  e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(["./","index.html"]);}).catch(function(){}));
-  self.skipWaiting();
-});
+self.addEventListener("install",function(){self.skipWaiting();});
 self.addEventListener("activate",function(e){
-  e.waitUntil(caches.keys().then(function(keys){return Promise.all(keys.filter(function(k){return k!==CACHE;}).map(function(k){return caches.delete(k);}));}));
-  self.clients.claim();
-});
-self.addEventListener("fetch",function(e){
-  if(e.request.method!=="GET")return;
-  e.respondWith(caches.match(e.request).then(function(r){return r||fetch(e.request).then(function(resp){return resp;}).catch(function(){return caches.match("./");});}));
+  e.waitUntil(
+    caches.keys().then(function(keys){return Promise.all(keys.map(function(k){return caches.delete(k);}));})
+    .then(function(){return self.registration.unregister();})
+    .then(function(){return self.clients.matchAll();})
+    .then(function(clients){clients.forEach(function(c){try{c.navigate(c.url);}catch(e){}});})
+  );
 });
