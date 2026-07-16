287cef3133ebc748be7d2c98f5813c0912744f2b
nonoskillers <noahbehar20@gmail.com>
2026-07-16 23:11:10 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = "rearise-v138";
+var CACHE = "rearise-v155";
 var CORE = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
 self.addEventListener("install", function (e) {
   self.skipWaiting();
